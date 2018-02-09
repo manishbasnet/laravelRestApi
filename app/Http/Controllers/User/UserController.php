79c229d41180d4e947cb7586630edf62b05fa980
manishbasnet <mbasnet911@gmail.com>
2018-02-09 20:49:53 +0545

    using implicit model binding for some methods

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -57,9 +57,8 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        $user = User::findOrFail($id);
 
         // return response()->json(['data' => $user], 200);
 
@@ -75,9 +74,8 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-        $user = User::findOrFail($id);
 
         $rules = [
             'email' => 'email|unique:users,email,' . $user->id,
@@ -130,9 +128,8 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        $user = User::findOrFail($id);
 
         $user->delete();
 
