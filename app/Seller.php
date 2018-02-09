ba4fb338ecde8dae42781a39f9829e0fd602055d
manishbasnet <mbasnet911@gmail.com>
2018-02-09 21:12:08 +0545

    resolving seller using global scopes

--- app/Seller.php
+++ app/Seller.php
@@ -1,11 +1,19 @@
 <?php
 
 namespace App;
-
+use App\Scopes\SellerScope;
 use App\Product;
 
 class Seller extends User
 {
+	protected static function boot()
+	{
+		parent::boot();
+
+		static::addGlobalScope(new SellerScope);
+
+	}
+
    public function products()
    {
    		return $this->hasMany(Product::class);
